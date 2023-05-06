e657f14cd3ced716fe72a8ac9084435bcbfe69d9
lee-insu <carpediem7760@gmail.com>
2023-05-06 15:07:36 +0900

    favicon edit

--- pages/push.tsx
+++ pages/push.tsx
@@ -8,12 +8,12 @@ const Push = () => {
       <div className="w-[88%] m-auto">
         <div className="my-9 space-y-0.5">
           <div className="text-center text-xl font-semibold text-gray-700">
-            공고 알림은 앱을 다운 받아야 받을 수 있어요🥹
+            알림을 받으려면 앱을 다운 받아야해요 🥹
           </div>
         </div>
         <div className="mt-10">
           <ul className="w-full m-auto flex flex-col space-y-8">
-            <li className="font-semibold text-lg bg-gray-100 text-center py-5 rounded-lg">
+            <li className="font-semibold text-lg border border-gray-400 text-center py-5 rounded-lg">
               {" "}
               저는 안드로이드에요
             </li>
